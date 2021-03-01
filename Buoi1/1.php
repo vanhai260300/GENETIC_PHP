f9258ae204be58aa06dd2f21d33fd298b1bb6000
Firstname Lastnamevanhai260300 <your_email@youremail.com>
2021-03-01 19:15:39 +0700

    buoi 1 2

--- Buoi1/1.php
+++ Buoi1/1.php
@@ -1,5 +1,16 @@
 <?php
  $age = 21;
  $name = "Nguyễn Văn Hải";
+ echo 4 . 5;
+ echo "</br>";
+ echo 4 + 5;
+ echo "</br>";
+ echo 4 - 5;
+ echo "</br>";
+ echo 4 * 5;
+ echo "</br>";
+ echo 4 / 5;
+ echo "</br>";
  echo "Name : " . $name . "</br>Age " . $age;
- echo "</br>hello world" ?>
+ echo "</br>hello world" 
+ ?>
